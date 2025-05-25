bc067e29a2e737425ad86edf93465744621b6e50
khushikukreti1 <149089248+khushikukreti1@users.noreply.github.com>
2025-05-25 07:24:49 +0000

    modified backend

---
+++ backend/uploads/test_input.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main() {
+    int a;
+    float b[3] = {1, 2};
+    a=a+5;
+    a = 5;
+    int y;
+    x=a;
+    c[0]=1;
+    b[2]=12;
+    b[5]=12; // out of bounds
+    gets(a);           // unsafe use
+    printf("Value: %d", b[2]); // valid
+    printf("Value: %d", b[5]); // out of bounds
+    int a;
+    return 0;
+}
